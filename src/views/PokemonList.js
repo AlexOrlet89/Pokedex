5f124fd3a6d157cd12b0be246272753dfcc68b7b
Alex Orlet <AlexOrlet89@gmail.com>
2022-04-26 14:44:43 -0700

    useEffect fetches data on load, sets pokemon state, and allows our pokemon to map on screen

--- src/views/PokemonList.js
+++ src/views/PokemonList.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchPokemon } from '../services/pokemon';
 
 export default function PokemonList() {
+  //'.map is not a funciton' means you're trying to map an object, you should be mapping and array!!!
   const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchPokemon();
+      setPokemon(data);
+    };
+    fetchData();
+  }, []);
+
   //   fetchPokemon();
-  const pokemon = fetchPokemon();
   return (
     <>
       <div>PokemonList</div>
-      {/* <ul>
+      <ul>
         {pokemon.map((poke) => (
           <li key={poke.id}>{poke.pokemon} </li>
         ))}
-      </ul> */}
+      </ul>
     </>
   );
 }
