eba9f5294d338cd7b663b3115c06ddd22da56ff3
Alex Orlet <AlexOrlet89@gmail.com>
2022-04-26 17:59:12 -0700

    planned out app.test.js, might be too meaty for me, but we will see

---
+++ src/App.test.js
@@ -0,0 +1,5 @@
+//we need to make a test that will render both the search bar and the pokemonlist, aka we are rendering the <App /> . this gonna be hard, we are going to use the character list demo pinned in chrome rn. goodluck tomorrow alex.
+
+test(
+  'search should filter our pokemon, updating our state and returning one specific pokemon'
+);
